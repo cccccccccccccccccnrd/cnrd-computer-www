7aa70a95d972d4ab93c4212e53847264f731bb3d
cnrd <hello@conradweise.com>
2019-10-27 11:03:45 +0100

    pic

--- public/main.js
+++ public/main.js
@@ -1,4 +1,23 @@
+console.log(`%c:---)`, 'padding: 5px; background: blue; color: white;')
+
+const name = document.getElementById('name')
+const conrad = document.getElementById('conrad')
 const buttons = [].slice.call(document.getElementsByClassName('button'))
+
+document.addEventListener('mousemove', (event) => {
+  if (event.target.id === 'name') {
+    conrad.style.left = `${ event.clientX + 15 }px`
+    conrad.style.top = `${ event.clientY + 15 }px`
+  }
+})
+
+name.addEventListener('mouseenter', (event) => {
+  conrad.style.display = 'block'
+})
+
+name.addEventListener('mouseleave', (event) => {
+  conrad.style.display = 'none'
+})
 
 buttons.forEach(element => {
   element.addEventListener('click', function () {
