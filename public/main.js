21c6a300daf768523ef21de819009ac09df6f71c
cnrd <hello@conradweise.com>
2020-10-21 21:59:01 +0200

    else {

--- public/main.js
+++ public/main.js
@@ -1,5 +1,9 @@
 console.log(`%c:---)`, 'padding: 5px; background: blue; color: white;')
 
+const name = document.querySelector('#name')
+const conrad = document.querySelector('#conrad')
+const projs = [].slice.call(document.querySelectorAll('.proj'))
+const switchh = document.querySelector('#switch')
 const project = new URLSearchParams(window.location.search).get('project')
 
 if (project) {
@@ -7,14 +11,19 @@ if (project) {
 }
 
 function open (project) {
-  document.querySelector(`#${ project }`).classList.toggle('show')
+  const element = document.querySelector(`#${ project }`)
+
+  if (element) {
+    element.classList.toggle('show')
+  } else {
+    switchh.classList.toggle('active')
+    projs.forEach((element) => {
+      element.classList.toggle('show')
+    })
+  }
+  
   window.history.pushState({}, {}, '/')
 }
-
-const name = document.querySelector('#name')
-const conrad = document.querySelector('#conrad')
-const projs = [].slice.call(document.querySelectorAll('.proj'))
-const switchh = document.querySelector('#switch')
 
 switchh.addEventListener('click', (event) => {
   switchh.classList.toggle('active')
