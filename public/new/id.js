b548eca7a1f1600145214a91b52688bff2e67eeb
cnrd <hello@conradweise.com>
2021-11-23 12:53:00 +0100

    let id

--- public/new/id.js
+++ public/new/id.js
@@ -1,10 +1,16 @@
+let id
+
+document.querySelector('#button').addEventListener('click', () => {
+  window.open(`https://www.youtube.com/watch?v=${id}`, '_blank')
+})
+
 function getId () {
   const chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz-_'
   return Array.apply(null, { length: 11 }).map(() => chars.charAt(Math.floor(Math.random() * chars.length))).join('')
 }
 
 function generate () {
-  const id = getId()
+  id = getId()
   document.querySelector('#id').innerText = id
 }
 
